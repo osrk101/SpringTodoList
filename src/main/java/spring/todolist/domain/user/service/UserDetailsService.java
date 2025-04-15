c648f1a5adf53a8bc6c7a00f812916048382affa
N <N@RYSEN52600>
2025-04-15 14:41:56 +0900

    完了

--- src/main/java/spring/todolist/domain/user/service/UserDetailsService.java
+++
@@ -1,9 +0,0 @@
-package spring.todolist.domain.user.service;
-
-import org.springframework.security.core.userdetails.UserDetails;
-
-public interface UserDetailsService {
-
-	public UserDetails loadUserByUsername();
-
-}
